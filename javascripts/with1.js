a693319253dc5e7754aa31a4ce8e492764557b6f
shigemk2 <i.am.shige@gmail.com>
2015-03-11 23:01:44 +0900

    fix indent

--- javascripts/with1.js
+++ javascripts/with1.js
@@ -5,9 +5,9 @@ var r = 10;
 // ネームスペースを束縛して省略する
 // Pythonでいうところのfrom math import *
 with (Math) {
-    a = PI * r * r;
-    x = r * cos(PI);
-    y = r * sin(PI / 2);
+  a = PI * r * r;
+  x = r * cos(PI);
+  y = r * sin(PI / 2);
 }
 
 // a = Math.PI * r * r;
