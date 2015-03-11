c4d124fc4fe407349a2d80241a1cebd45f7bb72a
shigemk2 <i.am.shige@gmail.com>
2015-03-12 00:49:11 +0900

    switch

---
+++ javascripts/switch1.js
@@ -0,0 +1,15 @@
+var a = 11;
+switch(a % 4) {
+  case 1:
+  console.log("one");
+  break;
+  case 2:
+  console.log("two");
+  break;
+  case 3:
+  console.log("three");
+  break;
+  default:
+  console.log("other");
+  break;
+}
