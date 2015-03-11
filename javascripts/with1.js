1685a815962df8eff2d0b2c9e95f1ef6e167343b
shigemk2 <i.am.shige@gmail.com>
2015-03-11 22:52:45 +0900

    with

--- javascripts/with1.js
+++ javascripts/with1.js
@@ -2,11 +2,17 @@
 var a, x, y;
 var r = 10;
 
+// ネームスペースを束縛して省略する
+// Pythonでいうところのfrom math import *
 with (Math) {
     a = PI * r * r;
     x = r * cos(PI);
     y = r * sin(PI / 2);
 }
+
+// a = Math.PI * r * r;
+// x = r * Math.cos(Math.PI);
+// y = r * Math.sin(Math.PI / 2);
 
 console.log(a);
 console.log(x);
