59089f070ba41c84f84f0578b19f983a8c950574
shigemk2 <i.am.shige@gmail.com>
2015-03-25 23:02:45 +0900

    Add JS

--- javascripts/swtich2.js
+++ javascripts/swtich2.js
@@ -1,27 +1,26 @@
 var a = 11;
-// a % 4 = exprRef
-// switchblock
+
 switch(a % 4) {
-// caseblock
-// caseclause
 case 1:
   console.log("one");
   break;
-// caseclause
 case 2:
   console.log("two");
   break;
-// caseclause
 case 3:
+  loop1:
+  for( var i=0; i<7; i++ ){
+    console.log('i: ' + i);
+    loop2:
+    for( var j=0; j<7; j++ ) {
+      console.log('j: ' + j);
+      break loop1;
+    }
+  }
   console.log("three");
   break;
-// defaultclause
 default:
   console.log("other");
   break;
-// caseblock
 }
-// switchblock
 
-// ---- R(GetValue(exprRef)) ----
-// exprRefは構文木
